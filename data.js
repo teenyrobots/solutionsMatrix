31896721b8e8d1b35984ea76621d6106caa4bb49
Joyce <hellojoycerice@gmail.com>
2018-09-12 18:04:49 -0700

    adds functionality to work like the v1 wireframes

--- data.js
+++ data.js
@@ -4,14 +4,91 @@ var data = [
 
     {
         slug: "b",
-        title: "this one has a title",
-        interventions: ["here's one", "here's another", "and a third"]
+        title: "ownership or stewardship of outlet",
+        interventions: [
+            "minority ownership of outlet or media company",
+            "diversity of investors ",
+            "diversity of board",
+            "public media",
+            "community media",
+            "incubators  & accelerators"
+        ]
     },
 
     {
         slug: "c",
-        title: "title number 2",
-        interventions: ["intervention 1", "intervention 2", "intervention 3", "intervention 4"]
+        title: "journalism and leadership education",
+        interventions: [
+            "primary",
+            "secondary",
+            "vocational",
+            "on-the-job",
+            "continuing",
+            "mid-career"
+        ]
+    },
+
+    {
+        slug: "d",
+        title: "hiring",
+        interventions: [
+            "internships",
+            "recruitment",
+            "early pipeline",
+            "freelancers",
+            "contractors",
+            "staff"
+        ]
+    },
+
+    {
+        slug: "d",
+        title: "stuff stuff stuff okay",
+        interventions: [
+            "internships",
+            "recruitment",
+            "early pipeline",
+            "freelancers",
+            "contractors",
+            "staff"
+        ]
+    },
+
+    {
+        slug: "d",
+        title: "desert planet",
+        interventions: [
+            "internships",
+            "recruitment",
+            "early pipeline",
+            "freelancers",
+            "contractors",
+            "staff"
+        ]
+    },
+
+    {
+        slug: "d",
+        title: "we got the text",
+        interventions: [
+            "internships",
+            "recruitment",
+            "early pipeline",
+            "freelancers",
+            "contractors",
+            "staff"
+        ]
     }
 
+]
+
+var colors = [
+    "#dda934",
+    "#e29a46",
+    "#e78c58",
+    "#ec7d6a",
+    "#f06e7d",
+    "#f55f8f",
+    "#fa51a1",
+    "#ff42b3"
 ]
--- script.js
+++ script.js
@@ -2,13 +2,30 @@
 
 $(document).ready(function(){
 
-    $(".tile").click(function(){
-        $(this).css("background-color", "red")
-    });
-
     // k we can do shit with the data okay, just to prove it
     for (i = 0; i < data.length; i++) {
-        console.log(data[i].interventions);
+        $(".tileContainer").append(
+            '<div class="tile" style="background-color:'+colors[i]+'"><h2 style="background-color:'+colors[i]+'">' + data[i].title + '</h2><div class="tileInfo">'+ data[i].interventions + '</div></div>'
+        );
     }
 
-})
+    $(".tile").click(function(){
+        $(this).toggleClass('tile explodedTile');
+        $(':nth-child(2)', this).toggle();
+    })
+
+});
+
+
+
+
+
+// $(".tile").click(function(){
+//     $(this).attr("class", "explodedTile");
+//     console.log("ya clicked on a tile");
+// });
+//
+// $(".explodedTile").click(function(){
+//     $(this).attr("class", "tile");
+//     console.log("ya clicked on a EXPLODED tile");
+// });
