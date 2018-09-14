21a441c1a8c705c6fd6b712cd5464e63b074d0b1
Joyce <hellojoycerice@gmail.com>
2018-09-13 17:49:23 -0700

    tidies things up

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     // k we can do shit with the data okay, just to prove it
     for (i = 0; i < data.length; i++) {
         $(".tileContainer").append(
-            '<div class="tile" id="'+data[i].slug+'"><h2>' + data[i].title + '</h2><div class="tileInfo">'+ intervene(data[i].interventions) + '</div></div>'
+            '<div class="tile" id="'+data[i].slug+'"><h2>' + data[i].title + '</h2><div class="tileInfo">'+ intervene(data[i].interventions) + examples(data[i].examples) + '</div></div>'
         );
         $('#'+data[i].slug).css("background-color", colors[i]);
     }
@@ -13,10 +13,20 @@ $(document).ready(function(){
     function intervene(x) {
         var myStuff = [];
         for (j = 0; j < x.length; j++){
-            myStuff.push("<p>"+x[j]+"</p>");
+            myStuff.push("<h3>"+x[j]+"</h3>");
         }
         console.log(myStuff.join(" "));
         return myStuff.join(" ");
+    }
+
+    function examples(x) {
+        if (x) {
+            var myExamples = [];
+            for (k = 0; k < x.length; k++){
+                myExamples.push("<a href=''>"+x[k]+"</a>");
+            }
+            return myExamples.join(" ");
+        }
     }
 
     $(".tile").click(function(){
